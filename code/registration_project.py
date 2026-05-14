756f11777186b47339a9ee9bb8de27512358e176
llamirhosseinll <awmirhossein5@gmail.com>
2026-05-14 22:27:48 +0200

    done until align imgaes with affine_MI intensity gradient optimization

--- code/registration_project.py
+++ code/registration_project.py
@@ -5,6 +5,7 @@ Project code for image registration topics.
 import numpy as np
 import matplotlib.pyplot as plt
 import registration as reg
+import registration_util as util
 from IPython.display import display, clear_output
 
 
@@ -84,3 +85,121 @@ def intensity_based_registration_demo():
         learning_curve.set_ydata(similarity)
 
         display(fig)
+
+
+# def optimize(fun, x, num_iter, mu):
+
+#     similarity = np.full((num_iter,1), np.nan)
+
+#     for k in np.arange(num_iter):
+#         print(f'iteration {k+1}/{num_iter}')
+#         g = reg.ngradient(fun, x)
+#         x+= g*mu
+        
+#         similarity[k] = fun(x)
+
+#     return x, similarity
+def optimize(fun, x, num_iter, mu, tol=1e-6):
+
+    similarity = np.full((num_iter,1), np.nan)
+
+    S_old = fun(x)
+
+    for k in range(num_iter):
+
+        print(f'iteration {k+1}/{num_iter}')
+
+        x_old = x.copy()
+
+        g = reg.ngradient(fun, x)
+
+        x += g * mu
+
+        S_new = fun(x)
+
+        similarity[k] = S_new
+
+        # stopping criteria
+
+        param_change = np.linalg.norm(x - x_old)
+
+        sim_change = abs(S_new - S_old)
+
+        if param_change < tol or sim_change < tol:
+
+            print(f'Converged at iteration {k+1}')
+
+            break
+
+        S_old = S_new
+
+    return x, similarity
+
+def intensity_based_registration(I, Im, method='rigid_cc', num_iter = 200, mu = 0.001):
+    match method:
+        case 'rigid_cc':
+            fun = lambda x: reg.rigid_corr(I, Im, x, return_transform = False)
+            x = np.array([0.]*3)
+            y_lim = (0,1)
+            SCALING = 100
+        case 'affine_cc':
+            fun = lambda x: reg.affine_corr(I, Im,  x, return_transform=False)
+            x = np.array([0.]*7)
+            y_lim = (0,1)
+            SCALING = 100
+        case 'affine_mi':
+            fun = lambda x: reg.affine_mi(I, Im, x, return_transform=False)
+            x = np.array([0., 1., 1., 0., 0., 0., 0.])
+            y_lim = (0,5)
+            SCALING = 100
+        case _:
+            print('invalid method chosen')
+            return
+    
+    
+
+
+    x, S = optimize(fun, x, num_iter, mu)
+    print(f"final similarity: {S[-1][0]:.4f}")
+
+    if len(x) == 3:
+        T = reg.rotate(x[0])
+        Th = util.t2h(T, x[1:]*SCALING)
+    else:
+        T = reg.rotate(x[0]).dot(reg.scale(x[1], x[2])).dot(reg.shear(x[3], x[4]))
+        Th = util.t2h(T, x[5:]*SCALING)
+    
+    Im_t, _ = reg.image_transform(Im, Th)
+    
+
+
+
+    ### displaying the graphs
+
+    fig = plt.figure(figsize=(14,6))
+
+    # fixed and moving image, and parameters
+    ax1 = fig.add_subplot(121)
+
+    # fixed image
+    im1 = ax1.imshow(I)
+    # moving image
+    im2 = ax1.imshow(Im_t, alpha=0.7)
+    # parameters
+    txt = ax1.text(0.3, 0.95,
+        np.array2string(x, precision=5, floatmode='fixed'),
+        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+        transform=ax1.transAxes)
+    
+    # 'learning' curve
+    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=y_lim)
+
+    iterations = np.arange(1, num_iter+1)
+
+    ax2.plot(iterations, S, lw=2)
+    ax2.set_xlabel('Iteration')
+    ax2.set_ylabel('Similarity')
+    ax2.grid()
+
+
+
